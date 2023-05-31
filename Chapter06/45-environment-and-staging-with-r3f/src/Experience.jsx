9471b0479b7ebabf49b819dc5d211963f86309fe
Mathieu van Leemputten <mathieuvanleemputten@hotmail.com>
2023-05-31 17:56:32 +0200

    45

--- Chapter06/45-environment-and-staging-with-r3f/src/Experience.jsx
+++ Chapter06/45-environment-and-staging-with-r3f/src/Experience.jsx
@@ -1,8 +1,21 @@
 import { useFrame } from '@react-three/fiber'
-import { BakeShadows, useHelper, OrbitControls } from '@react-three/drei'
+import {
+  Sky,
+  ContactShadows,
+  RandomizedLight,
+  softShadows,
+  BakeShadows,
+  useHelper,
+  OrbitControls,
+  SoftShadows,
+  AccumulativeShadows,
+  Environment,
+  Lightformer,
+} from '@react-three/drei'
 import { useRef } from 'react'
 import { Perf } from 'r3f-perf'
 import * as THREE from 'three'
+import { useControls } from 'leva'
 
 export default function Experience() {
   const directionalLight = useRef()
@@ -10,12 +23,78 @@ export default function Experience() {
   const cube = useRef()
 
   useFrame((state, delta) => {
+    // const time = state.clock.elapsedTime
+    // cube.current.position.x = 2 + Math.sin(time)
     cube.current.rotation.y += delta * 0.2
+  })
+
+  const { color, opacity, blur } = useControls('contact shadows', {
+    color: '#000000',
+    opacity: { value: 0.5, min: 0, max: 1 },
+    blur: { value: 1, min: 0, max: 10 },
+  })
+
+  const { sunPosition } = useControls('sky', {
+    sunPosition: { value: [1, 2, 3] },
+  })
+
+  const { envMapIntensity } = useControls('environment map', {
+    envMapIntensity: { value: 3.5, min: 0, max: 12 },
   })
 
   return (
     <>
-      <BakeShadows />
+      <Environment background>
+        <color
+          args={['black']}
+          attach="background"
+        />
+        {/* <mesh
+          position-z={-5}
+          scale={10}>
+          <planeGeometry />
+          <meshBasicMaterial color={[10, 0, 0]} />
+        </mesh> */}
+        <Lightformer />
+      </Environment>
+
+      {/* <BakeShadows /> */}
+
+      {/* <SoftShadows
+        frustum={3.75}
+        size={0.005}
+        near={9.5}
+        samples={17}
+        rings={11}
+      /> */}
+
+      {/* <AccumulativeShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        color="#316d39"
+        opacity={0.8}
+        frames={1000}
+        temporal
+        blend={100}>
+        <RandomizedLight
+          amount={8}
+          position={[1, 2, 3]}
+          ambient={0.5}
+          intensity={1}
+          bias={0.001}
+        />
+      </AccumulativeShadows> */}
+
+      <ContactShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        resolution={128}
+        far={5}
+        color={color}
+        opacity={opacity}
+        blur={blur}
+        frames={1}
+      />
 
       <color
         args={['ivory']}
@@ -26,9 +105,9 @@ export default function Experience() {
 
       <OrbitControls makeDefault />
 
-      <directionalLight
+      {/* <directionalLight
         ref={directionalLight}
-        position={[1, 2, 3]}
+        position={sunPosition}
         intensity={1.5}
         castShadow
         shadow-mapSize={[1024, 1024]}
@@ -38,15 +117,20 @@ export default function Experience() {
         shadow-camera-right={5}
         shadow-camera-left={-5}
         shadow-camera-bottom={-5}
-      />
+      /> */}
 
-      <ambientLight intensity={0.5} />
+      {/* <ambientLight intensity={0.5} /> */}
+
+      {/* <Sky sunPosition={sunPosition} /> */}
 
       <mesh
         castShadow
         position-x={-2}>
         <sphereGeometry />
-        <meshStandardMaterial color="orange" />
+        <meshStandardMaterial
+          color="orange"
+          envMapIntensity={envMapIntensity}
+        />
       </mesh>
 
       <mesh
@@ -55,16 +139,21 @@ export default function Experience() {
         position-x={2}
         scale={1.5}>
         <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
+        <meshStandardMaterial
+          color="mediumpurple"
+          envMapIntensity={envMapIntensity}
+        />
       </mesh>
 
       <mesh
-        receiveShadow
         position-y={-1}
         rotation-x={-Math.PI * 0.5}
         scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+        <meshStandardMaterial
+          color="greenyellow"
+          envMapIntensity={envMapIntensity}
+        />
       </mesh>
     </>
   )
