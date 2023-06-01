bb41fb5369c4201ed4087079ec3e18a3c39dc5cb
Mathieu van Leemputten <mathieuvanleemputten@hotmail.com>
2023-06-01 14:39:33 +0200

    48

---
+++ Chapter06/48-portal-scene-with-r3f/src/Experience.jsx
@@ -0,0 +1,46 @@
+import { Sparkles, Center, useTexture, useGLTF, OrbitControls } from '@react-three/drei'
+
+export default function Experience() {
+  const { nodes } = useGLTF('./model/portal.glb')
+
+  const bakedTexture = useTexture('./model/baked.jpg')
+  bakedTexture.flipY = false
+
+  return (
+    <>
+      <color
+        args={['#030202']}
+        attach="background"
+      />
+
+      <OrbitControls makeDefault />
+      <Center>
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={bakedTexture} />
+        </mesh>
+        <mesh
+          geometry={nodes.poleLightA.geometry}
+          position={nodes.poleLightA.position}>
+          <meshBasicMaterial color={'#ffffe5'} />
+        </mesh>
+        <mesh
+          geometry={nodes.poleLightB.geometry}
+          position={nodes.poleLightB.position}>
+          <meshBasicMaterial color={'#ffffe5'} />
+        </mesh>
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}></mesh>
+
+        <Sparkles
+          size={6}
+          scale={[4, 2, 4]}
+          position-y={1}
+          speed={0.2}
+          count={40}
+        />
+      </Center>
+    </>
+  )
+}
