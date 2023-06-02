ac80d5d6e5e6f7d66f4b504a206687f75fcf7f77
Mathieu van Leemputten <mathieuvanleemputten@hotmail.com>
2023-06-02 10:58:26 +0200

    50

---
+++ Chapter06/50-post-processing-with-r3f/src/Experience.jsx
@@ -0,0 +1,51 @@
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import { EffectComposer, Vignette } from '@react-three/postprocessing'
+
+export default function Experience() {
+  return (
+    <>
+      <EffectComposer multisampling={8}>
+        <Vignette
+          offset={0.3}
+          darkness={0.9}
+        />
+      </EffectComposer>
+
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+
+      <directionalLight
+        castShadow
+        position={[1, 2, 3]}
+        intensity={1.5}
+      />
+      <ambientLight intensity={0.5} />
+
+      <mesh
+        castShadow
+        position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh
+        castShadow
+        position-x={2}
+        scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+    </>
+  )
+}
