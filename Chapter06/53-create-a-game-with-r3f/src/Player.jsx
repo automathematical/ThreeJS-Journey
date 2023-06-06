8f31baab3e224ee8c65e8e4a6f6d5856d3f1f1d5
Mathieu van Leemputten <mathieuvanleemputten@hotmail.com>
2023-06-06 13:44:27 +0200

    53

---
+++ Chapter06/53-create-a-game-with-r3f/src/Player.jsx
@@ -0,0 +1,38 @@
+import { RigidBody } from '@react-three/rapier'
+import { useFrame } from '@react-three/fiber'
+import { useKeyboardControls } from '@react-three/drei'
+import { useRef } from 'react'
+
+export default function Player() {
+  const body = useRef()
+  const [subscribeKeys, getKeys] = useKeyboardControls()
+
+  useFrame(() => {
+    const { forward, backward, leftward, rightward } = getKeys()
+
+    const impulse = { x: 0, y: 0, z: 0 }
+    const torque = { x: 0, y: 0, z: 0 }
+
+    body.current.applyImpulse(impulse)
+    body.current.applyTorqueImpulse(torque)
+  })
+
+  return (
+    <>
+      <RigidBody
+        ref={body}
+        colliders="ball"
+        restitution={0.2}
+        friction={1}
+        position={[0, 1, 0]}>
+        <mesh castShadow>
+          <icosahedronGeometry args={[0.3, 1]} />
+          <meshStandardMaterial
+            flatShading
+            color="mediumpurple"
+          />
+        </mesh>
+      </RigidBody>
+    </>
+  )
+}
