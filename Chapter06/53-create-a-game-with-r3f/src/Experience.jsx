c1ed8c01d8b140a66eeb4468f5bc665609934fce
Mathieu van Leemputten <mathieuvanleemputten@hotmail.com>
2023-06-08 12:07:26 +0200

    53

--- Chapter06/53-create-a-game-with-r3f/src/Experience.jsx
+++ Chapter06/53-create-a-game-with-r3f/src/Experience.jsx
@@ -3,8 +3,11 @@ import { Physics, Debug } from '@react-three/rapier'
 import Lights from './Lights.jsx'
 import { Level } from './Level.jsx'
 import Player from './Player.jsx'
+import useGame from './stores/useGame.jsx'
 
 export default function Experience() {
+  const blocksCount = useGame((state) => state.blocksCount)
+
   return (
     <>
       {/* <OrbitControls makeDefault /> */}
@@ -12,7 +15,7 @@ export default function Experience() {
       <Physics>
         {/* <Debug /> */}
         <Lights />
-        <Level />
+        <Level count={blocksCount} />
         <Player />
       </Physics>
     </>
